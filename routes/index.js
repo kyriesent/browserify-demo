8c928b6e0d68fb9bc3bec8fa6be19d2808ec372b
David Jsa <kiesent@gmail.com>
2014-10-12 18:18:13 -0400

    remove all clientside functionality

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,16 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+exports.index = function(req, res) {
+  res.render('index', { title: 'Browserify Demo' });
 };
+
+exports.submitEmail = function(req, res) {
+  console.log(req.body.email);
+  var email = req.body.email;
+  if (isValidEmail(email)) {
+    res.send('Nice job!')
+  } else {
+    res.send('Your email fails. HIT THE BACK BUTTON')
+  }
+}
